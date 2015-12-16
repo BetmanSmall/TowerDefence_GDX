279ff5477bdf46018c62b69fc0d96f5328977ca3
Andrey Andrianov <descogle@gmail.com>
2015-12-16 10:26:01 +0400

    gdx
    some gl20 interfase and map rendering

--- desktop/src/com/betmansmall/game/desktop/DesktopLauncher.java
+++ desktop/src/com/betmansmall/game/desktop/DesktopLauncher.java
@@ -2,16 +2,15 @@ package com.betmansmall.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.betmansmall.game.OrthographicCameraController;
 import com.betmansmall.game.TowerDefence;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Tower Defence";
-		config.width = 1600;
-		config.height = 900;
-//		config.useGL30 = true;
+		config.width = 800;
+		config.height = 600;
+		config.useGL30 = true;
 		new LwjglApplication(new TowerDefence(), config);
 //		new LwjglApplication(new OrthographicCameraController(), config);
 	}
