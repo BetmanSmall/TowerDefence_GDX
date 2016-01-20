bfd8c7c505ec9eab1fab0bbaa7c65575ab02c2f3
Andrey Andrianov <descogle@gmail.com>
2016-01-21 01:54:10 +0300

    Add actors and scene

--- core/src/com/betmansmall/game/MainMenuScreen.java
+++ core/src/com/betmansmall/game/MainMenuScreen.java
@@ -6,77 +6,84 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class MainMenuScreen implements Screen {
 
-    private OrthographicCamera camera;
     private Texture background;
     private Texture buttons;
+    private TextureRegion menuButton;
+
     private SpriteBatch mainmenubatch;
     private TowerDefence towerDefence;
-    private Actor
+    MenuButtons menuButtons;
+    private Stage mmStage;
+
 
     public MainMenuScreen(TowerDefence towerDefence){
         this.towerDefence = towerDefence;
-        this.camera = new OrthographicCamera();
-        this.camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+    }
+
+    class MenuButtons extends Actor{
+        @Override
+        public void draw(Batch batch, float parentAlpha) {
+            batch.draw(menuButton,getX(),getY(),getWidth(),getHeight());
+        }
     }
 
     @Override
     public void show() {
-        mainmenubatch = new SpriteBatch();
         background = new Texture(Gdx.files.internal("img/background.jpg"));
         buttons = new Texture(Gdx.files.internal("img/buttons.png"));
+        menuButton = new TextureRegion(buttons, 0, 0, 360, 600);
+        mainmenubatch = new SpriteBatch();
+        mmStage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(mmStage);
 
-        Gdx.input.setInputProcessor(new InputAdapter() {
-            @Override
-            public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-                Gdx.app.log("Example", "touch started at (" + screenX + ", " + screenY + ")");
-                //towerDefence.setScreen(new GameScreen(towerDefence));
-                touchDownAnalizer(screenX, screenY);
-                return false;
+        menuButtons = new MenuButtons();
+        menuButtons.setPosition(mmStage.getWidth() / 2 - buttons.getWidth() / 2, 0);
+        menuButtons.setSize(buttons.getWidth(), buttons.getHeight());
+        mmStage.addActor(menuButtons);
+
+        menuButtons.addListener(new InputListener() {
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                System.out.println("down");
+                touchDownAnalizer(x,y);
+                return true;
             }
         });
-    }
+        }
 
-    private void touchDownAnalizer(int x, int y){
-        if(x>=780 && x<= 1140 && y>=442 && y<=600) //if Play button pressed
-        {
+    private void touchDownAnalizer(float x, float y){
+        if(true){
             towerDefence.setScreen(new GameScreen(towerDefence));
-            Gdx.app.log("Playbutton was pressed!","Coordinates were:"+ x + ", " + y);
-        }
-
-        if(x>=780 && x<= 1140 && y>=259 && y<=417) //if About button pressed
-        {
-            Gdx.app.log("About was pressed!", "Coordinates were:" + x + ", " + y);
-        }
-        if(x>=780 && x<= 1140 && y>=73 && y<=231) //if Exit button pressed
-        {
-            Gdx.app.log("Exit was pressed!", "Coordinates were:" + x + ", " + y);
-            //towerDefence.closeApplication();
         }
     }
 
     @Override
     public void render(float delta) {
-        Gdx.gl20.glClearColor(0, 0, 0, 1);
         Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        mainmenubatch.begin();
-        mainmenubatch.draw(background,0,0);
-        mainmenubatch.draw(buttons, Gdx.graphics.getWidth() / 2 - 180, 0, 360, 600);
-        mainmenubatch.end();
+
+        menuButtons.setPosition(mmStage.getWidth()/2 - buttons.getWidth()/2, 0);
+        mmStage.getBatch().begin();
+        mmStage.getBatch().draw(background, 0, 0);
+        mmStage.getBatch().end();
+        mmStage.draw();
+        mmStage.act(delta);
         //Gdx.app.log("GameScreen FPS", (1/delta) + "");
     }
 
     @Override
     public void resize(int width, int height) {
-        camera.viewportWidth = width;
-        camera.viewportHeight = height;
-        camera.position.set(800f, 0f, 100f);
-        camera.update();
+        mmStage.getViewport().update(width, height, true);
     }
 
     @Override
@@ -94,6 +101,6 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void dispose() {
-        mainmenubatch.dispose();
+        mmStage.dispose();
     }
 }
