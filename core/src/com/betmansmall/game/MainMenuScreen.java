a0f7ffa2d2e50e7e2450c4953f46e08207313b52
BetmanSmall <betmansmall@mail.ru>
2019-04-15 12:26:53 +0300

    delete useless classes thx Dima

--- core/src/com/betmansmall/game/MainMenuScreen.java
+++ core/src/com/betmansmall/game/MainMenuScreen.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
