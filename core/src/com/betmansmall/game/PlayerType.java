3e3ffd83b370ca3ccbe83fe6c3fce6e3bff05e70
BetmanSmall <betmansmall@mail.ru>
2019-10-30 19:27:05 +0300

    PlayerType implements Serializable

--- core/src/com/betmansmall/game/PlayerType.java
+++ core/src/com/betmansmall/game/PlayerType.java
@@ -1,10 +1,12 @@
 package com.betmansmall.game;
 
+import java.io.Serializable;
+
 /**
  * TODO mb should be only PLAYER and SPECTATOR
  * @author Alexander on 28.10.2019.
  */
-public enum PlayerType {
+public enum PlayerType implements Serializable {
     SERVER,
     CLIENT,
     SPECTATOR
