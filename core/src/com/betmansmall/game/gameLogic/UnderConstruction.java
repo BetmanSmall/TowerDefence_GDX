67472336cbe88398a03df997a94f69b1fe4beda5
ART <wazilinik@gmail.com>
2020-04-10 00:43:11 +0300

    Алгоритм Брезенхема

--- core/src/com/betmansmall/game/gameLogic/UnderConstruction.java
+++ core/src/com/betmansmall/game/gameLogic/UnderConstruction.java
@@ -14,6 +14,7 @@ public class UnderConstruction {
     public TemplateForTower templateForTower;
     public Array<Integer> coorsX;
     public Array<Integer> coorsY;
+    public int x,y,dx, dy, incx, incy, pdx, pdy, es, el, err;
 
     public UnderConstruction(TemplateForTower templateForTower) {
         Gdx.app.log("UnderConstruction::UnderConstruction()", "-- templateForTower:" + templateForTower);
@@ -62,47 +63,88 @@ public class UnderConstruction {
         if(state == 1 && templateForTower != null) {
             coorsX.clear();
             coorsY.clear();
-            int towerSize = templateForTower.size;
-            int deltaX = 0, deltaY = 0;
-            if(towerSize != 1) {
-                if(towerSize%2 == 0) {
-                    deltaX = towerSize/2;
-                    deltaY = (towerSize/2)-1;
-                } else {
-                    deltaX = towerSize/2;
-                    deltaY = towerSize/2;
-                }
+//            int towerSize = templateForTower.size;
+//            int deltaX = 0, deltaY = 0;
+//            if(towerSize != 1) {
+//                if(towerSize%2 == 0) {
+//                    deltaX = towerSize/2;
+//                    deltaY = (towerSize/2)-1;
+//                } else {
+//                    deltaX = towerSize/2;
+//                    deltaY = towerSize/2;
+//                }
+//            }
+//            int tmpX = startX - deltaX;
+//            int tmpY = startY - deltaY;
+//            if(endY == tmpY || (endY < (tmpY+towerSize) && endY > tmpY)) {
+//                if(endX >= tmpX) {
+//                    for(int currX = tmpX+towerSize; currX <= endX; currX+=towerSize) {
+//                        this.coorsX.add(currX+deltaX);
+//                        this.coorsY.add(tmpY+deltaY);
+//                    }
+//                } else {
+//                    for(int currX = tmpX-towerSize; currX > endX-towerSize; currX-=towerSize) {
+//                        this.coorsX.add(currX+deltaX);
+//                        this.coorsY.add(tmpY+deltaY);
+//                    }
+//                }
+//            } else if(endX == tmpX || endX < (tmpX+towerSize) && endX > tmpX) {
+//                if(endY >= tmpY) {
+//                    for(int currY = tmpY+towerSize; currY <= endY; currY+=towerSize) {
+//                        this.coorsX.add(tmpX+deltaX);
+//                        this.coorsY.add(currY+deltaY);
+//                    }
+//                } else {
+//                    for(int currY = tmpY-towerSize; currY > endY-towerSize; currY-=towerSize) {
+//                        this.coorsX.add(tmpX+deltaX);
+//                        this.coorsY.add(currY+deltaY);
+//                    }
+//                }
+//            }
+            dx = endX - startX;
+            incx = sign(dx);
+            dy = endY - startY;
+            incy = sign(dy);
+
+            if (dx < 0) dx = -dx;
+            if (dy < 0) dy = -dy;
+
+            if (dx > dy) {
+                pdx = incx;
+                pdy = 0;
+                es = dy;
+                el = dx;
+            } else {
+                pdx = 0;
+                pdy = incy;
+                es = dx;
+                el = dy;
             }
-            int tmpX = startX-deltaX;
-            int tmpY = startY-deltaY;
-            if(endY == tmpY || (endY < (tmpY+towerSize) && endY > tmpY)) {
-                if(endX >= tmpX) {
-                    for(int currX = tmpX+towerSize; currX <= endX; currX+=towerSize) {
-                        this.coorsX.add(currX+deltaX);
-                        this.coorsY.add(tmpY+deltaY);
-                    }
+            x = startX;
+            y = startY;
+            err = el / 2;
+            this.coorsX.add(x);
+            this.coorsY.add(y);
+            for (int t = 0; t < el; t++) {
+                err -= es;
+                if (err < 0) {
+                    err += el;
+                    x += incx;
+                    y += incy;
                 } else {
-                    for(int currX = tmpX-towerSize; currX > endX-towerSize; currX-=towerSize) {
-                        this.coorsX.add(currX+deltaX);
-                        this.coorsY.add(tmpY+deltaY);
-                    }
+                    x += pdx;
+                    y += pdy;
                 }
-            } else if(endX == tmpX || endX < (tmpX+towerSize) && endX > tmpX) {
-                if(endY >= tmpY) {
-                    for(int currY = tmpY+towerSize; currY <= endY; currY+=towerSize) {
-                        this.coorsX.add(tmpX+deltaX);
-                        this.coorsY.add(currY+deltaY);
-                    }
-                } else {
-                    for(int currY = tmpY-towerSize; currY > endY-towerSize; currY-=towerSize) {
-                        this.coorsX.add(tmpX+deltaX);
-                        this.coorsY.add(currY+deltaY);
-                    }
-                }
+                this.coorsX.add(x);
+                this.coorsY.add(y);
             }
             return true;
         }
         return false;
+    }
+
+    private int sign (int x) {
+        return (x > 0) ? 1 : (x < 0) ? -1 : 0;
     }
 
     public boolean clearStartCoors() {
